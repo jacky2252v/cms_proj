979e67562a95d4b3118ac34688714b3e4389f4d9
jacky2252v <vivekbaraiya000@gmail.com>
2025-02-25 14:09:41 +0530

    Add Connection page with LanguageSelector and MongoDBCredentialsForm components

---
+++ src/app/[slug]/page.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import LanguageSelector from '@/components/LanguageSelector/LanguageSelector';
+import { MongoDBCredentialsForm } from '@/components/MongoDBConnection/MongoDBConnection';
+import { use } from 'react';
+
+export default function Connection({ params }: { params: Promise<{ slug: string }> }) {
+    const { slug } = use(params);
+    if (slug === "language") {
+        return (
+            <main className="min-h-screen flex items-center justify-center p-4">
+                <LanguageSelector />
+            </main>
+        );
+    } else if (slug === "marquee") {
+        return (
+            <main className="min-h-screen flex items-center justify-center p-4">
+                <MongoDBCredentialsForm />
+            </main>
+        );
+    }
+    return null;
+}
